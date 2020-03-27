dca1ac1d442ff3831170aa18de2b34ee852d7e8c
Robert Virkus <robert.virkus@open-xchange.com>
2020-03-27 18:12:07 +0100

    increase  robustness of date parsing Some implementations do not follow the specification requirements for timezone offset notation. The adapted code now tries to recognize the timezone and base the date parsing on that timezone.

--- lib/encodings.dart
+++ lib/encodings.dart
@@ -393,18 +393,18 @@ Date and time values occur in several header fields.  This section
     var yearText = dateText.substring(0, spaceIndex);
     dateText = dateText.substring(spaceIndex + 1).trimLeft();
     spaceIndex = dateText.indexOf(' ');
-    if (spaceIndex == -1) {
-      return null;
-    }
-    var timeText = dateText.substring(0, spaceIndex);
+    var timeText = dateText;
     var zoneText = '+0000';
-    if (dateText.length > spaceIndex) {
-      dateText = dateText.substring(spaceIndex + 1).trim();
-      spaceIndex = dateText.indexOf(' ');
-      if (spaceIndex == -1) {
-        zoneText = dateText;
-      } else {
-        zoneText = dateText.substring(0, spaceIndex);
+    if (spaceIndex != -1) {
+      timeText = dateText.substring(0, spaceIndex);
+      if (dateText.length > spaceIndex) {
+        dateText = dateText.substring(spaceIndex + 1).trim();
+        spaceIndex = dateText.indexOf(' ');
+        if (spaceIndex == -1) {
+          zoneText = dateText;
+        } else {
+          zoneText = dateText.substring(0, spaceIndex);
+        }
       }
     }
     var dayOfMonth = int.tryParse(dayText);
@@ -442,8 +442,221 @@ Date and time values occur in several header fields.  This section
           !(zoneText.startsWith('+') || zoneText.startsWith('-'))) {
         zoneText = '+' + zoneText;
       } else {
-        print('invalid time zone $zoneText in $original');
-        return null;
+        // source: https://en.wikipedia.org/wiki/List_of_time_zone_abbreviations
+        var timeZonesByName = <String, String>{
+          'GMT':
+              '+0000', // Greenwich Mean Time - most often this will be used by non-compliant implenentations
+          'ACDT': '+1030', // Australian Central Daylight Savings Time
+          'ACDT': '+0930', // Australian Central Standard Time
+          'ACT': '-0500', // Acre Time
+          'ACWST':
+              '+0845', // Australian Central Western Standard Time (unofficial)
+          'ADT': '-0300', // Atlantic Daylight Time
+          'AEDT': '+1100', // 	Australian Eastern Daylight Savings Time
+          'AEST': '+1000', // Australian Eastern Standard Time
+          'AET':
+              '+1000', // 	Australian Eastern Time - can also apparently be +1100
+          'AFT': '+0430', // Afghanistan Time
+          'AKDT': '-0800', // Alaska Daylight Time
+          'AKST': '-0900', // 	Alaska Standard Time
+          'ALMT': '+0600', // Alma-Ata Time
+          'AMST': '-0300', // Amazon Summer Time (Brazil)
+          'AMT':
+              '+0400', // can be Amazon Time or Armenia Time. Since Brasil has other time zones we assume Armenia Time
+          'ANAT': '+1200', // Anadyr Time
+          'AQTT': '+0500', // Aqtobe Time
+          'ART': '-0300', // Argentina Time
+          'AST':
+              '+0300', // Arabia Standar Time, could also be Atlantic Standard Time
+          'AWST': '+0800', // Australian Western Standard Time
+          'AZOST': '+0000', // Azores Summer Time
+          'AZOT': '+0100', // Azores Standard Time
+          'AZT': '+0400', // Azerbaijan Time
+          'BDT': '+0800', // Brunei Time
+          'BIOT': '+0600', // British Indian Ocean Time
+          'BIT': '-1200', // Baker Island Time
+          'BOT': '-0400', // Bolivia Time
+          'BRST': '-0200', // Brasília Summer Time
+          'BRT': '-0300', // Brasília Time
+          'BST':
+              '+0600', // Bangladesh Standard Time, but could also be Bougainville Standard Time +1100
+          'BTT': '+0600', // 	Bhutan Time
+          'CAT': '+0200', // Central Africa Time
+          'CCT': '+0630', // Cocos Islands Time
+          'CDT':
+              '-0500', // Central Daylight Time (North America) - could also be Cuba Daylight Time -0400
+          'CEST': '+0200', // Central European Summer Time (Cf. HAEC)
+          'CET': '+0100', // Central European Time
+          'CHADT': '+1345', // Chatham Daylight Time
+          'CHAST': '+1245', // Chatham Standard Time
+          'CHOT': '+0800', // Choibalsan Standard Time
+          'CHOST': '+0900', // Choibalsan Summer Time
+          'CHST': '+1000', // Chuuk Time
+          'CIST': '-0800', // Clipperton Island Standard Time
+          'CIT': '+0800', // Central Indonesia Time
+          'CKT': '-1000', // Cook Island Time
+          'CLST': '-0300', // Chile Summer Time
+          'CLT': '-0400', // Chile Standard Time
+          'COST': '-0400', // Colombia Summer Time
+          'COT': '-0500', // Colombia Time
+          'CST':
+              '-0600', // Central Standard Time (North America), could also be China Standard Time +0800 or Cuba Standard Time -0500
+          'CT': '+0800', // China Time
+          'CVT': '-0100', // Cape Verde Time
+          'CWST':
+              '+0845', // Central Western Standard Time (Australia) unofficial
+          'CXT': '+0700', // Christmas Island Time
+          'DAVT': '+0700', // Davis Time
+          'DDUT': '+1000', // Dumont d'Urville Time
+          'DFT': '+0100', // AIX-specific equivalent of Central European Time
+          'EASST': '-0500', // Easter Island Summer Time
+          'EAST': '-0600', // Easter Island Standard Time
+          'EAT': '+0300', // East Africa Time
+          'ECT':
+              '-0500', //  Ecuador Time, could also be Eastern Caribbean Time -0400
+          'EDT': '-0400', // 	Eastern Daylight Time (North America)
+          'EEST': '+0300', // 	Eastern European Summer Time
+          'EET': '+0200', // Eastern European Time
+          'EGST': '+0000', // Eastern Greenland Summer Time
+          'EGT': '-0100', // Eastern Greenland Time
+          'EIT': '+0900', // Eastern Indonesian Time
+          'EST': '-0500', // Eastern Standard Time (North America)
+          'FET': '+0300', // Further-eastern European Time
+          'FJT': '+1200', // Fiji Time
+          'FKST': '-0300', // Falkland Islands Summer Time
+          'FKT': '-0400', // 	Falkland Islands Time
+          'FNT': '-0200', // Fernando de Noronha Time
+          'GALT': '-0600', // Galápagos Time
+          'GAMT': '-0900', // Gambier Islands Time
+          'GET': '+0400', // 	Georgia Standard Time
+          'GFT': '-0300', // French Guiana Time
+          'GILT': '+1200', // Gilbert Island Time
+          'GIT': '-0900', // Gambier Island Time
+          'GST':
+              '+0400', // Gulf Standard Time, could also be South Georgia and the South Sandwich Islands Time -0200
+          'GYT': '-0400', // Guyana Time
+          'HDT': '-0900', // Hawaii–Aleutian Daylight Time
+          'HAEC':
+              '+0200', // Heure Avancée d'Europe Centrale French-language name for CEST
+          'HST': '-1000', // 	Hawaii–Aleutian Standard Time
+          'HKT': '+0800', // Hong Kong Time
+          'HMT': '+0500', // Heard and McDonald Islands Time
+          'HOVST': '+0800', // Hovd Summer Time
+          'HOVT': '+0700', // Hovd Time
+          'ICT': '+0700', // Indochina Time
+          'IDLW': '-1200', // International Day Line West time zone
+          'IDT': '+0300', // Israel Daylight Time
+          'IOT': '+0300', // Indian Ocean Time
+          'IRDT': '+0430', // Iran Daylight Time
+          'IRKT': '+0800', // Irkutsk Time
+          'IRST': '+0330', // Iran Standard Time
+          'IST':
+              '+0530', // 	Indian Standard Time, could also be Irish Standard Time +0100 or Israel Standard Time +0200
+          'JST': '+0900', // Japan Standard Time
+          'KALT': '+0200', // Kaliningrad Time
+          'KGT': '+0600', // Kyrgyzstan Time
+          'KOST': '+1100', // Kosrae Time
+          'KRAT': '+0700', // Krasnoyarsk Time
+          'KST': '+0900', // Korea Standard Time
+          'LHST':
+              '+1030', // Lord Howe Standard Time, could also be Lord Howe Summer Time +1100
+          'LINT': '+1400', // Line Islands Time
+          'MAGT': '+1200', // Magadan Time
+          'MART': '-0930', // Marquesas Islands Time
+          'MAWT': '+0500', // Mawson Station Time
+          'MDT': '-0600', // Mountain Daylight Time (North America)
+          'MET': '+0100', // Middle European Time Same zone as CET
+          'MEST': '+0200', // Middle European Summer Time Same zone as CEST
+          'MHT': '+1200', // Marshall Islands Time
+          'MIST': '+1100', // Macquarie Island Station Time
+          'MIT': '-0930', // 	Marquesas Islands Time
+          'MMT': '+0630', // Myanmar Standard Time
+          'MSK': '+0300', // 	Moscow Time
+          'MST':
+              '-0700', // Mountain Standard Time (North America), could also be Malaysia Standard Time +0800
+          'MUT': '+0400', // Mauritius Time
+          'MVT': '+0500', // Maldives Time
+          'MYT': '+0800', // Malaysia Time
+          'NCT': '+1100', // New Caledonia Time
+          'NDT': '-0230', // Newfoundland Daylight Time
+          'NFT': '+1100', // Norfolk Island Time
+          'NOVT': '+0700', // Novosibirsk Time
+          'NPT': '+0545', // Nepal Time
+          'NST': '-0330', // 	Newfoundland Standard Time
+          'NT': '-0330', // Newfoundland Time
+          'NUT': '-1100', // Niue Time
+          'NZDT': '+1300', // New Zealand Daylight Time
+          'NZST': '+1200', // New Zealand Standard Time
+          'OMST': '+0600', // Omsk Time
+          'ORAT': '+0500', // Oral Time
+          'PDT': '-0700', // 	Pacific Daylight Time (North America)
+          'PET': '-0500', // Peru Time
+          'PETT': '+1200', // Kamchatka Time
+          'PGT': '+1000', // Papua New Guinea Time
+          'PHOT': '+1300', // Phoenix Island Time
+          'PHT': '+0800', // Philippine Time
+          'PKT': '+0500', // 	Pakistan Standard Time
+          'PMDT': '-0200', // Saint Pierre and Miquelon Daylight Time
+          'PMST': '-0300', // 	Saint Pierre and Miquelon Standard Time
+          'PONT': '+1100', // Pohnpei Standard Time
+          'PST':
+              '-0800', // Pacific Standard Time (North America), could also be Philippine Standard Time +0800
+          'PYST': '-0300', // Paraguay Summer Time
+          'PYT': '-0400', // Paraguay Time
+          'RET': '+0400', // Réunion Time
+          'ROTT': '-0300', // Rothera Research Station Time
+          'SAKT': '+1100', // 	Sakhalin Island Time
+          'SAMT': '+0400', // Samara Time
+          'SAST': '+0200', // South African Standard Time
+          'SBT': '+1100', // Solomon Islands Time
+          'SCT': '+0400', // Seychelles Time
+          'SDT': '-1000', // Samoa Daylight Time
+          'SGT': '+0800', // Singapore Time
+          'SLST': '+0530', // Sri Lanka Standard Time
+          'SRET': '+1100', // Srednekolymsk Time
+          'SRT': '-0300', // Suriname Time
+          'SST': '-1100', // Samoa Standard Time
+          'SST': '+0800', // Singapore Standard Time
+          'SYOT': '+0300', // Showa Station Time
+          'TAHT': '-1000', // Tahiti Time
+          'THA': '+0700', // Thailand Standard Time
+          'TFT': '+0500', // French Southern and Antarctic Time
+          'TJT': '+0500', // Tajikistan Time
+          'TKT': '+1300', // Tokelau Time
+          'TLT': '+0900', // Timor Leste Time
+          'TMT': '+0500', // Turkmenistan Time
+          'TRT': '+0300', // Turkey Time
+          'TOT': '+1300', // Tonga Time
+          'TVT': '+1200', // Tuvalu Time
+          'ULAST': '+0900', // 	Ulaanbaatar Summer Time
+          'ULAT': '+0800', // Ulaanbaatar Standard Time
+          'UTC': '+0000', // 	Coordinated Universal Time
+          'UYST': '-0200', // Uruguay Summer Time
+          'UYT': '-0300', // Uruguay Standard Time
+          'UZT': '+0500', // Uzbekistan Time
+          'VET': '-0400', // Venezuelan Standard Time
+          'VLAT': '+1000', // Vladivostok Time
+          'VOLT': '+0400', // Volgograd Time
+          'VOST': '+0600', // Vostok Station Time
+          'VUT': '+1100', // 	Vanuatu Time
+          'WAKT': '+1200', // Wake Island Time
+          'WAST': '+0200', // West Africa Summer Time
+          'WAT': '+0100', // West Africa Time
+          'WEST': '+0100', // Western European Summer Time
+          'WET': '+0000', // Western European Time
+          'WIT': '+0700', // Western Indonesian Time
+          'WGST': '-0200', // West Greenland Summer Time
+          'WGT': '-0300', // West Greenland Time
+          'WST': '+0800', // Western Standard Time (North America)
+          'YAKT': '+0900', // Yakutsk Time
+          'YEKT': '+0500', // Yekaterinburg Time
+        };
+        var zoneOffset = timeZonesByName[zoneText];
+        if (zoneOffset == null) {
+          print('invalid time zone [$zoneText] in $original');
+          return null;
+        }
+        zoneText = zoneOffset;
       }
     }
     var timeZoneHours = int.tryParse(zoneText.substring(1, 3));
--- test/encodings_test.dart
+++ test/encodings_test.dart
@@ -4,72 +4,112 @@ import 'package:test/test.dart';
 import 'package:enough_mail/encodings.dart';
 
 void main() {
-  test('encodings.quoted-printable header', () {
-    var input =
-        '=?utf-8?Q?Chat=3A?==?utf-8?Q?_?=oh=?utf-8?Q?_?==?utf-8?Q?hi=2C?='
-        '=?utf-8?Q?__?=how=?utf-8?Q?_?=do=?utf-8?Q?_?=you=?utf-8?Q?_?==?utf-8?Q?do=3F?==?utf-8?Q?_?==?utf-8?Q?=3A-)?=';
-    expect(
-        EncodingsHelper.decodeAny(input), 'Chat: oh hi,  how do you do? :-)');
+  group('Quoted Printable decoding', () {
+    test('encodings.quoted-printable header', () {
+      var input =
+          '=?utf-8?Q?Chat=3A?==?utf-8?Q?_?=oh=?utf-8?Q?_?==?utf-8?Q?hi=2C?='
+          '=?utf-8?Q?__?=how=?utf-8?Q?_?=do=?utf-8?Q?_?=you=?utf-8?Q?_?==?utf-8?Q?do=3F?==?utf-8?Q?_?==?utf-8?Q?=3A-)?=';
+      expect(
+          EncodingsHelper.decodeAny(input), 'Chat: oh hi,  how do you do? :-)');
+    });
+
+    test('encodings.quoted-printable header no direct start', () {
+      var input =
+          ' =?utf-8?Q?Chat=3A?==?utf-8?Q?_?=oh=?utf-8?Q?_?==?utf-8?Q?hi=2C?='
+          '=?utf-8?Q?__?=how=?utf-8?Q?_?=do=?utf-8?Q?_?=you=?utf-8?Q?_?==?utf-8?Q?do=3F?==?utf-8?Q?_?==?utf-8?Q?=3A-)?=';
+      expect(EncodingsHelper.decodeAny(input),
+          ' Chat: oh hi,  how do you do? :-)');
+    });
+
+    test('encoding.iso-8859-1 quoted printable', () {
+      var input = '=?iso-8859-1?Q?Bj=F6rn?= Tester <btester@domain.com>';
+      expect(EncodingsHelper.decodeAny(input),
+          'Björn Tester <btester@domain.com>');
+    });
+
+    test('encoding.iso-8859-1 quoted printable not at start', () {
+      var input = 'Tester =?iso-8859-1?Q?Bj=F6rn?= <btester@domain.com>';
+      expect(EncodingsHelper.decodeAny(input),
+          'Tester Björn <btester@domain.com>');
+    });
+
+    test('encoding.UTF-8.QuotedPrintable with several codes', () {
+      var input = '=?utf-8?Q?=E2=80=93?=';
+      expect(EncodingsHelper.decodeAny(input),
+          isNotNull); // this results in a character - which for some reasons cannot be pasted as Dart code
+    });
+    test('encoding.US-ASCII.QuotedPrintable', () {
+      var input = '=?US-ASCII?Q?Keith_Moore?= <moore@cs.utk.edu>';
+      expect(
+          EncodingsHelper.decodeAny(input), 'Keith Moore <moore@cs.utk.edu>');
+    });
   });
 
-  test('encodings.quoted-printable header no direct start', () {
-    var input =
-        ' =?utf-8?Q?Chat=3A?==?utf-8?Q?_?=oh=?utf-8?Q?_?==?utf-8?Q?hi=2C?='
-        '=?utf-8?Q?__?=how=?utf-8?Q?_?=do=?utf-8?Q?_?=you=?utf-8?Q?_?==?utf-8?Q?do=3F?==?utf-8?Q?_?==?utf-8?Q?=3A-)?=';
-    expect(
-        EncodingsHelper.decodeAny(input), ' Chat: oh hi,  how do you do? :-)');
+  group('Base64 decoding', () {
+    test('encoding.iso-8859-1 base64 directly repeated', () {
+      var input =
+          '=?ISO-8859-1?B?SWYgeW91IGNhbiByZWFkIHRoaXMgeW8=?==?ISO-8859-2?B?dSB1bmRlcnN0YW5kIHRoZSBleGFtcGxlLg==?=';
+      expect(EncodingsHelper.decodeAny(input),
+          'If you can read this you understand the example.');
+    });
+
+    test('encoding.UTF-8.Base64 with non-devidable-by-four base64 text', () {
+      expect(EncodingsHelper.decodeBase64('8J+UkA', utf8), '🔐');
+      var input = '=?utf-8?B?8J+UkA?= New Access Request - local.name';
+      expect(EncodingsHelper.decodeAny(input),
+          '🔐 New Access Request - local.name');
+    });
+
+    test('encoding.US-ASCII.Base64', () {
+      var input = '=?US-ASCII?B?S2VpdGggTW9vcmU?= <moore@cs.utk.edu>';
+      expect(
+          EncodingsHelper.decodeAny(input), 'Keith Moore <moore@cs.utk.edu>');
+      input = '=?US-ASCII?B?S2VpdGggTW9vcmU=?= <moore@cs.utk.edu>';
+      expect(
+          EncodingsHelper.decodeAny(input), 'Keith Moore <moore@cs.utk.edu>');
+    });
   });
 
-  test('encoding.iso-8859-1 quoted printable', () {
-    var input = '=?iso-8859-1?Q?Bj=F6rn?= Tester <btester@domain.com>';
-    expect(
-        EncodingsHelper.decodeAny(input), 'Björn Tester <btester@domain.com>');
-  });
-
-  test('encoding.iso-8859-1 quoted printable not at start', () {
-    var input = 'Tester =?iso-8859-1?Q?Bj=F6rn?= <btester@domain.com>';
-    expect(
-        EncodingsHelper.decodeAny(input), 'Tester Björn <btester@domain.com>');
-  });
-
-  test('encoding.iso-8859-1 base64 directly repeated', () {
-    var input = '=?ISO-8859-1?B?SWYgeW91IGNhbiByZWFkIHRoaXMgeW8=?==?ISO-8859-2?B?dSB1bmRlcnN0YW5kIHRoZSBleGFtcGxlLg==?=';
-    expect(
-        EncodingsHelper.decodeAny(input), 'If you can read this you understand the example.');
-  });
-  
-
-  test('encoding.UTF-8.QuotedPrintable with several codes', () {
-    var input = '=?utf-8?Q?=E2=80=93?=';
-    expect(EncodingsHelper.decodeAny(input),
-        isNotNull); // this results in a character - which for some reasons cannot be pasted as Dart code
-  });
-
-  test('encoding.UTF-8.Base64 with non-devidable-by-four base64 text', () {
-    expect(EncodingsHelper.decodeBase64('8J+UkA', utf8), '🔐');
-    var input = '=?utf-8?B?8J+UkA?= New Access Request - local.name';
-    expect(
-        EncodingsHelper.decodeAny(input), '🔐 New Access Request - local.name');
-  });
-
-  test('encoding.US-ASCII.QuotedPrintable', () {
-    var input = '=?US-ASCII?Q?Keith_Moore?= <moore@cs.utk.edu>';
-    expect(EncodingsHelper.decodeAny(input), 'Keith Moore <moore@cs.utk.edu>');
-  });
-
-  test('encoding.US-ASCII.Base64', () {
-    var input = '=?US-ASCII?B?S2VpdGggTW9vcmU?= <moore@cs.utk.edu>';
-    expect(EncodingsHelper.decodeAny(input), 'Keith Moore <moore@cs.utk.edu>');
-    input = '=?US-ASCII?B?S2VpdGggTW9vcmU=?= <moore@cs.utk.edu>';
-    expect(EncodingsHelper.decodeAny(input), 'Keith Moore <moore@cs.utk.edu>');
-  });
-
-  test('decodeDate', () {
-    expect(EncodingsHelper.decodeDate('11 Feb 2020 22:45 +0000'),
-        DateTime.utc(2020, 2, 11, 22, 45));
-    expect(EncodingsHelper.decodeDate('11 Feb 2020 22:45 +0100'),
-        DateTime.utc(2020, 2, 11, 23, 45));
-    expect(EncodingsHelper.decodeDate('11 Feb 2020 22:45 +0200'),
-        DateTime.utc(2020, 2, 12, 0, 45));
+  group('decode dates', () {
+    test('decodeDate simple', () {
+      expect(EncodingsHelper.decodeDate('11 Feb 2020 22:45 +0000'),
+          DateTime.utc(2020, 2, 11, 22, 45));
+      expect(EncodingsHelper.decodeDate('11 Feb 2020 22:45 +0100'),
+          DateTime.utc(2020, 2, 11, 23, 45));
+      expect(EncodingsHelper.decodeDate('11 Feb 2020 22:45 +0200'),
+          DateTime.utc(2020, 2, 12, 0, 45));
+    });
+    test('decodeDate with weekday', () {
+      expect(EncodingsHelper.decodeDate('Tue, 11 Feb 2020 22:45 +0000'),
+          DateTime.utc(2020, 2, 11, 22, 45));
+      expect(EncodingsHelper.decodeDate('Tue, 11 Feb 2020 22:45 +0100'),
+          DateTime.utc(2020, 2, 11, 23, 45));
+      expect(EncodingsHelper.decodeDate('Tue, 11 Feb 2020 22:45 +0200'),
+          DateTime.utc(2020, 2, 12, 0, 45));
+    });
+    test('decodeDate with timezone name', () {
+      expect(EncodingsHelper.decodeDate('11 Feb 2020 22:45 +0000 GMT'),
+          DateTime.utc(2020, 2, 11, 22, 45));
+      expect(EncodingsHelper.decodeDate('11 Feb 2020 22:45 +0100 CET'),
+          DateTime.utc(2020, 2, 11, 23, 45));
+      expect(EncodingsHelper.decodeDate('11 Feb 2020 22:45 +0200 EET'),
+          DateTime.utc(2020, 2, 12, 0, 45));
+    });
+    test('decodeDate with timezone name and weekday', () {
+      expect(EncodingsHelper.decodeDate('Tue, 11 Feb 2020 22:45 +0000 GMT'),
+          DateTime.utc(2020, 2, 11, 22, 45));
+      expect(EncodingsHelper.decodeDate('Tue, 11 Feb 2020 22:45 +0100 CET'),
+          DateTime.utc(2020, 2, 11, 23, 45));
+      expect(EncodingsHelper.decodeDate('11 Feb 2020 22:45 +0200 EET'),
+          DateTime.utc(2020, 2, 12, 0, 45));
+    });
+    test('decodeDate without timezone offset', () {
+      expect(EncodingsHelper.decodeDate('Thu, 26 Mar 2020 18:11:28'),
+          DateTime.utc(2020, 3, 26, 18, 11, 28));
+    });
+    test('decodeDate without timezone offset but timezone name', () {
+      expect(EncodingsHelper.decodeDate('Thu, 26 Mar 2020 18:11:28 GMT'),
+          DateTime.utc(2020, 3, 26, 18, 11, 28));
+    });
   });
 }
