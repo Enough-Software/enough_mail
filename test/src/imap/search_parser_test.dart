2e324f56e58f6cbc687221924658bbefc9217679
Robert Virkus <robert.virkus@open-xchange.com>
2020-05-31 22:54:54 +0200

    improve code style

--- test/src/imap/search_parser_test.dart
+++ test/src/imap/search_parser_test.dart
@@ -1,4 +1,4 @@
-import 'package:enough_mail/enough_mail.dart';
+import 'package:enough_mail/imap/response.dart';
 import 'package:enough_mail/src/imap/all_parsers.dart';
 import 'package:enough_mail/src/imap/imap_response_line.dart';
 import 'package:enough_mail/src/imap/imap_response.dart';
