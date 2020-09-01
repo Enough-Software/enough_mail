1a668980fedbb603a65d08736bc2b97742e6f888
A. Zulli <musashinmen+github@gmail.com>
2020-09-01 14:51:54 +0200

    Removed custom helper methods

--- lib/imap/resource_limit.dart
+++ lib/imap/resource_limit.dart
@@ -10,13 +10,4 @@ class ResourceLimit {
   final int usageLimit;
 
   ResourceLimit(this.name, this.currentUsage, this.usageLimit);
-
-  /// Helper method for determining a unlimited resource.
-  bool get isUnlimited => (usageLimit ?? -1) < 0;
-
-  /// Returs the [usageLimit] as percentile of [currentUsage].
-  double usageAsPercentage() {
-    if (usageLimit < 0) return 0;
-    return (currentUsage) / (usageLimit * 1.0) * 100.0;
-  }
 }
