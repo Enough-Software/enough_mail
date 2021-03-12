3bb1fa865c6161854bffc5e1b5c2a05020230e48
A. Zulli <70531066+azulli@users.noreply.github.com>
2021-03-12 10:55:02 +0100

    Split message/rfc822 nested MimeParts correctly.

---
+++ lib/src/util/byte_utils.dart
@@ -0,0 +1,23 @@
+class ByteUtils {
+  /// Finds a [sequence] of bytes into a [pool], returns the starting position or -1 if not found.
+  static int findSequence(final List<int> pool, final List<int> sequence) {
+    // The pool size is reduced by the sequence length to avoid the eventual overflow
+    final dataSize = pool.length - sequence.length;
+    final needleSize = sequence.length;
+    var result = -1;
+    for (var pos = 0; pos < dataSize; pos++) {
+      var matchFound = true;
+      for (var j = 0; j < needleSize; j++) {
+        if (pool[pos + j] != sequence[j]) {
+          matchFound = false;
+          break;
+        }
+      }
+      if (matchFound) {
+        result = pos;
+        break;
+      }
+    }
+    return result;
+  }
+}
