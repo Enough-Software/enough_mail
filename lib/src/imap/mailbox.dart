e3d9fbc4a7105c2f3f8f203c22a8d6659ce73949
Mohamed adel tawfiq <mohamedun9@gmail.com>
2023-03-13 05:41:16 +0200

    test inbox

--- lib/src/imap/mailbox.dart
+++ lib/src/imap/mailbox.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:collection/collection.dart' show IterableExtension;
 
 import '../codecs/modified_utf7_codec.dart';
@@ -86,6 +88,7 @@ class Mailbox {
     this.extendedData = const {},
   })  : name = _modifiedUtf7Codec.decodeText(encodedName),
         path = _modifiedUtf7Codec.decodeText(encodedPath) {
+    log('dola from enoughMail ${name.toLowerCase()}');
     if (!isInbox && name.toLowerCase() == 'inbox') {
       flags.add(MailboxFlag.inbox);
     }
