49298d5433b5bc33cd019a07e33da4b1a373c2ff
mohamed tawfiq <mohamedun9@gmail.com>
2023-08-24 05:11:09 +0300

    add mail box with new data

--- lib/src/mail/mail_client.dart
+++ lib/src/mail/mail_client.dart
@@ -1952,6 +1952,23 @@ class _IncomingImapClient extends _IncomingMailClient {
           enableCondStore: enableCondStore, qresync: quickReSync);
       _selectedMailbox = selectedMailbox;
       _threadData = null;
+
+
+
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.reload();
+      final mails = prefs.getString('mailboxes') ?? '';
+      if(mails.trim().isNotEmpty){
+        final List<dynamic> mailBoxList = jsonDecode(mails);
+        List<Mailbox> models = mailBoxList.map((e) => Mailbox.fromJson(e)).toList();
+        models.removeWhere((element) => element.encodedName == selectedMailbox.encodedName);
+        models.add(selectedMailbox);
+        final mailBoxes = jsonEncode(models.map((e) => e.toJson()).toList());
+        await prefs.setString('mailboxes', mailBoxes);
+      }
+
+
+
       return selectedMailbox;
     } on ImapException catch (e) {
       throw MailException.fromImap(mailClient, e, e.code);
